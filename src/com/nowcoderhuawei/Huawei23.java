ac41e90aed426cf43e8497d9647a87cf9172012f
CarYanG <yangy.cs.nju@gmail.com>
2016-04-23 20:21:11 +0800

    20160423

--- src/com/nowcoderhuawei/Huawei23.java
+++ src/com/nowcoderhuawei/Huawei23.java
@@ -14,25 +14,47 @@ public class Huawei23 {
             LinkedHashMap lhs=new LinkedHashMap();
             for(int i=0;i<a.length();i++){
                 if(lhs.containsKey(a.charAt(i))){
-                    lhs.put(a.charAt(i),lhs.get(a.charAt(i)+1));
+                    lhs.put(a.charAt(i),(int)lhs.get(a.charAt(i))+1);
                 }
                 else {
-                    lhs.put(a.charAt(i),0);
+                    lhs.put(a.charAt(i),1);
                 }
             }
-            List<Map.Entry<String, Integer>> infoIds =
-                    new ArrayList<Map.Entry<String, Integer>>(lhs.entrySet());
 
-            Collections.sort(infoIds, new Comparator<Map.Entry<String, Integer>>() {
-                @Override
-                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                    return o1.getValue().compareTo(o2.getValue());
+
+            List<Map.Entry<Character, Integer>> list = new LinkedList<Map.Entry<Character, Integer>>(lhs.entrySet());
+            Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>()
+            {
+                public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2)
+                {
+                    if(o2.getValue()!=null&&o1.getValue()!=null&&o2.getValue().compareTo(o1.getValue())<0){
+                        return 1;
+                    }else{
+                        return -1;
+                    }
+
                 }
             });
 
+           int min=list.get(0).getValue();
+            for(Character ch:a.toCharArray()){
+                if((int)lhs.get(ch)!=min){
 
-            int c=(int)lhs.values().toArray()[0];
-            System.out.println(c);
+                    System.out.print(ch);
+                }
+            }
+            System.out.println("");
+
+
+//
+//            for(Iterator<Map.Entry<Character, Integer>> ite = list.iterator(); ite.hasNext();) {
+//                Map.Entry<Character, Integer> map = ite.next();
+//                System.out.println("key-value: " + map.getKey() + "," + map.getValue());
+//            }
+//
+//
+//            int c=(int)lhs.values().toArray()[0];
+//            System.out.println(c);
 
         }
     }
@@ -41,7 +63,7 @@ public class Huawei23 {
 /**
  * 题目描述
 
- 实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
+ 实现删除字符串中出现次数最少的字符， 若多个字符出现次数一样，则都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
 
  输入描述:
  字符串只包含小写英文字母, 不考虑非法输入，输入的字符串长度小于等于20个字节。
@@ -57,3 +79,43 @@ public class Huawei23 {
  dd
 
  */
+
+/*
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String s = in.nextLine();
+            int[] a = new int[26];
+            for(int i=0;i<s.length();i++){
+                a[s.charAt(i)-97]++;
+            }
+            int min = 21;
+            //找出出现最小的次数
+            for(int i=0;i<26;i++){
+                if(a[i]<min && a[i]>0){
+                    min = a[i];
+                }
+            }
+            StringBuilder toBeDeleted = new StringBuilder();
+            for(int i=0;i<26;i++){
+                if(a[i]==min){
+                    char c = (char)('a'+i);
+                    toBeDeleted.append(c);
+                }
+            }
+            String str = toBeDeleted.toString();
+            StringBuilder res = new StringBuilder();
+            for(int i=0;i<s.length();i++){
+                if(!str.contains(s.charAt(i)+"")){
+                    res.append(s.charAt(i));
+                }
+            }
+            System.out.println(res);
+        }
+    }
+
+}
+*/
--- src/com/nowcoderhuawei/test.java
+++ src/com/nowcoderhuawei/test.java
@@ -1,5 +1,5 @@
 package com.nowcoderhuawei;
-
+import java.util.*;
 /**
  * Created by carl on 2016/4/15.
  */
@@ -12,9 +12,27 @@ public class test {
         System.out.println(a+"A");
     }
     public static void main(String args[]){
-         int a=1;
-        System.out.println(a);
-        new test().function();;
+        HashMap map_Data=new HashMap();
+        map_Data.put("A", "98");
+        map_Data.put("B", "50");
+        map_Data.put("C", "50");
+        map_Data.put("D", "25");
+        map_Data.put("E", "85");
+        System.out.println(map_Data);
+        List<Map.Entry<String, String>> list_Data = new ArrayList<Map.Entry<String, String>>(map_Data.entrySet());
+        Collections.sort(list_Data, new Comparator<Map.Entry<String, String>>()
+        {
+            public int compare(Map.Entry<String, String> o1, Map.Entry<String, String> o2)
+            {
+                if(o2.getValue()!=null&&o1.getValue()!=null&&o2.getValue().compareTo(o1.getValue())>0){
+                    return 1;
+                }else{
+                    return -1;
+                }
+
+            }
+        });
+        System.out.println(list_Data);
 
 
     }
