5b6500587b119f7229bbe5421f00ebab89e92356
yangy.cs.nju <yangy.cs.nju@gmail.com>
2016-08-13 02:01:40 +0800

    20160812

---
+++ src/com/swordtooffer/SwordOffer39.java
@@ -0,0 +1,15 @@
+package com.swordtooffer;
+
+/**
+ * Created by Administrator on 2016/8/13.
+ */
+public class SwordOffer39 {
+    public boolean IsBalanced_Solution(TreeNode root) {
+            return false;
+    }
+}
+
+/**
+ * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+
+ */
--- src/com/swordtooffer/SwordOffer58.java
+++ src/com/swordtooffer/SwordOffer58.java
@@ -1,6 +1,6 @@
 package com.swordtooffer;
 
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by carl on 2016/8/11.
@@ -11,33 +11,92 @@ public class SwordOffer58 {
     boolean isSymmetrical(TreeNode pRoot)
 
     {
-        //遍历一下这个树，利用一个栈作为辅助
-        Stack<TreeNode> treeNode=new Stack<>();
-        treeNode.push(pRoot);
+        //层析遍历这个树，分析每层的节点
+        Queue<TreeNode> treeNodeQueue=new LinkedList<TreeNode>();
+        if(pRoot!=null){
+            treeNodeQueue.add(pRoot);
+        }
 
-        while(treeNode.size()!=0){
-            TreeNode nowRoot=treeNode.pop();
-            if(nowRoot.left!=null || nowRoot.right!=null){
-                int leftnumber;
-                leftnumber= nowRoot.left==null? -1:nowRoot.left.val;
-                int rightnumber;
-                rightnumber= nowRoot.right==null? -1:nowRoot.right.val;
-
-                if(leftnumber!=rightnumber){
-                    return false;
+        while(treeNodeQueue.size()!=0){
+            int nowNumber=treeNodeQueue.size(); //记录当前队列的长度
+            List<String> treeNodeList=new ArrayList<String>();
+            for(int i=0;i<nowNumber;i++){
+                TreeNode temp=treeNodeQueue.poll();
+                if(temp.right!=null){
+                    treeNodeQueue.offer(temp.right);
+                    treeNodeList.add(temp.right.val+"");
 
                 }
+                else{
+                    treeNodeList.add("#");
+                }
+                if(temp.left!=null){
+                    treeNodeQueue.offer(temp.left);
+                    treeNodeList.add(temp.left.val+"");
+                }
+                else{
+                    treeNodeList.add("#");
+                }
+
             }
-            if(nowRoot.left!=null){
-                treeNode.push(nowRoot.left);
+
+            int low=0;
+            int high=treeNodeList.size()-1;
+            while(low<high) {
+                String a=treeNodeList.get(low++);
+                String b=treeNodeList.get(high--);
+                if (!(a.equals(b))) {
+                    return false;
+                }
             }
-            if(nowRoot.right!=null){
-                treeNode.push(nowRoot.right);
-            }
+//            if(treeNodeQueue.size()!=0){
+//                List<Integer> treeNodeList=new ArrayList<Integer>();
+//                Iterator iterator = treeNodeQueue.iterator();
+//                while(iterator.hasNext()){
+//                    TreeNode element = (TreeNode) iterator.next();
+//                    treeNodeList.add(element.val);
+//                }
+//
+//                if(treeNodeList.size()%2!=0){  //奇数，不对称
+//                    return false;
+//                }
+//                int low=0;
+//                int high=treeNodeList.size()-1;
+//                while(low<high){
+//                    if(treeNodeList.get(low++)!=treeNodeList.get(high--)){
+//                        return false;
+//
+//                    }
+//                }
+//            }
 
         }
 
+
         return true;
+    }
+
+    public static void main(String argsp[]){
+        TreeNode root=new TreeNode(5);
+        TreeNode left1=new TreeNode(5);
+        TreeNode left2=new TreeNode(5);
+        TreeNode left3=new TreeNode(5);
+
+        TreeNode right1=new TreeNode(5);
+        TreeNode right2=new TreeNode(5);
+        TreeNode right3=new TreeNode(5);
+
+        root.left=left1;
+        root.right=right1;
+
+        left1.left=left2;
+        left2.left=left3;
+
+        right1.right=right2;
+        right2.left=right3;
+
+        System.out.println(new SwordOffer58().isSymmetrical(root));
+
     }
 }
 
