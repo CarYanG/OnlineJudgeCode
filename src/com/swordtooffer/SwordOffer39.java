f48e5b2c46dbb91302d47d43d2afddf887659148
yangy.cs.nju <yangy.cs.nju@gmail.com>
2016-08-15 14:59:43 +0800

    20160815

--- src/com/swordtooffer/SwordOffer39.java
+++ src/com/swordtooffer/SwordOffer39.java
@@ -2,9 +2,12 @@ package com.swordtooffer;
 
 /**
  * Created by Administrator on 2016/8/13.
+ *
+ * 最好不要用递归，重复计算高度
  */
 public class SwordOffer39 {
     public boolean IsBalanced_Solution(TreeNode root) {
+
         if(root==null){
             return true;
         }
@@ -26,11 +29,32 @@ public class SwordOffer39 {
         if(root==null){
             return 0;
         }
-        if(root.left!=null||root.right!=null){
+        if(root.left==null&&root.right==null){
             return 1;
         }
 
-        return getHeight(root.left)>getHeight(root.right)? getHeight(root.left):getHeight(root.right);
+        return getHeight(root.left)>getHeight(root.right)? getHeight(root.left)+1:getHeight(root.right)+1;
+
+    }
+
+
+    public static void main(String args[]){
+        TreeNode a1=new TreeNode(1);
+        TreeNode a2=new TreeNode(2);
+        TreeNode a3=new TreeNode(3);
+        TreeNode a4=new TreeNode(4);
+        TreeNode a5=new TreeNode(5);
+        TreeNode a6=new TreeNode(6);
+
+        a1.left=a2;
+        a1.right=a3;
+        a2.left=a4;
+        a2.right=a5;
+        a5.left=a6;
+
+
+        System.out.println(new SwordOffer39().IsBalanced_Solution(a1));
+        System.out.println(new SwordOffer39().getHeight(a5));
 
     }
 }
